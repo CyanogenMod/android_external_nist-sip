2c0c45f5be273cc60cc5dad5bcbaeb0401bd2305
Hung-ying Tyan <tyanh@google.com>
2010-05-28 15:07:15 +0800

    SIP: remove WakeupTimer interface as it is used by SipServiceImpl with service context
    Change-Id: I97a0490140922733aaff5dbfd55127d0860ca9fe

--- src/com/android/sip/WakeupTimer.java
+++ src/com/android/sip/WakeupTimer.java
@@ -16,35 +16,76 @@
 
 package com.android.sip;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 /**
  * Timer that can schedule events to occur even when the device is in sleep.
  * Only used internally in this package.
  */
-interface WakeupTimer {
-    static abstract class Factory {
-        private static Factory sFactory;
+class WakeupTimer extends BroadcastReceiver {
+    private static final String TAG = "__SIP.WakeupTimer__";
+    private static final String EVENT_ID = "EventID";
 
-        /**
-         * Creates a timer object.
-         */
-        public abstract WakeupTimer createTimer();
+    private Context mContext;
+    private AlarmManager mAlarmManager;
 
-        public static Factory getInstance() {
-            if (sFactory == null) {
-                throw new RuntimeException("No factory is available");
-            }
-            return sFactory;
-        }
+    // runnable --> time to execute in SystemClock
+    private PriorityQueue<MyEvent> mEventQueue =
+            new PriorityQueue<MyEvent>(1, new MyEventComparator());
 
-        static void setInstance(Factory factory) {
-            sFactory = factory;
-        }
+    public WakeupTimer(Context context) {
+        mContext = context;
+        mAlarmManager = (AlarmManager)
+                context.getSystemService(Context.ALARM_SERVICE);
+
+        IntentFilter filter = new IntentFilter(getAction());
+        context.registerReceiver(this, filter);
     }
 
     /**
      * Stops the timer. No event can be scheduled after this method is called.
      */
-    void stop();
+    public synchronized void stop() {
+        mContext.unregisterReceiver(this);
+        for (MyEvent event : mEventQueue) {
+            mAlarmManager.cancel(event.mPendingIntent);
+        }
+        mEventQueue.clear();
+        mEventQueue = null;
+    }
+
+    private boolean stopped() {
+        if (mEventQueue == null) {
+            Log.w(TAG, "Timer stopped");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private MyEvent createMyEvent(long triggerTime, Runnable callback) {
+        MyEvent event = new MyEvent();
+        event.mTriggerTime = triggerTime;
+        event.mCallback = callback;
+
+        Intent intent = new Intent(getAction());
+        intent.putExtra(EVENT_ID, event.toString());
+        PendingIntent pendingIntent = event.mPendingIntent =
+                PendingIntent.getBroadcast(mContext, 0, intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        return event;
+    }
 
     /**
      * Sets a timer event.
@@ -53,12 +94,133 @@ interface WakeupTimer {
      * @param callback is called back when the timer goes off; the same
      *      callback can be specified in multiple timer events
      */
-    void set(long delay, Runnable callback);
+    public synchronized void set(long delay, Runnable callback) {
+        if (stopped()) return;
+
+        long t = SystemClock.elapsedRealtime();
+        long triggerTime = t + delay;
+        MyEvent event = createMyEvent(triggerTime, callback);
+
+        MyEvent firstEvent = mEventQueue.peek();
+        if (!mEventQueue.offer(event)) {
+            throw new RuntimeException("failed to add event: " + callback);
+        }
+        if (mEventQueue.peek() == event) {
+            if (firstEvent != null) {
+                mAlarmManager.cancel(firstEvent.mPendingIntent);
+            }
+            scheduleNext();
+        }
+
+        Log.v(TAG, " add event " + event + " scheduled at "
+                + showTime(triggerTime) + " at " + showTime(t)
+                + ", #events=" + mEventQueue.size());
+        printQueue();
+    }
 
     /**
      * Cancels all the timer events with the specified callback.
      *
      * @param callback the callback
      */
-    void cancel(Runnable callback);
+    public synchronized void cancel(Runnable callback) {
+        if (stopped()) return;
+
+        MyEvent firstEvent = mEventQueue.peek();
+        for (Iterator<MyEvent> iter = mEventQueue.iterator();
+                iter.hasNext();) {
+            MyEvent event = iter.next();
+            if (event.mCallback == callback) iter.remove();
+        }
+        if ((firstEvent != null) && (firstEvent.mCallback == callback)) {
+            mAlarmManager.cancel(firstEvent.mPendingIntent);
+            scheduleNext();
+        }
+    }
+
+    private void scheduleNext() {
+        if (stopped() || mEventQueue.isEmpty()) return;
+
+        MyEvent event = mEventQueue.peek();
+        mAlarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                event.mTriggerTime, event.mPendingIntent);
+    }
+
+    @Override
+    public synchronized void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (getAction().equals(action)) {
+            MyEvent firstEvent = mEventQueue.peek();
+            String eventId = intent.getStringExtra(EVENT_ID);
+            if (firstEvent.toString().equals(eventId)) {
+                execute();
+            } else {
+                // log the unexpected event schedule
+                Log.d(TAG, "time's up for " + eventId
+                        + " but event q is:");
+                printQueue();
+            }
+        }
+    }
+
+    private void printQueue() {
+        int count = 0;
+        for (MyEvent event : mEventQueue) {
+            Log.d(TAG, "     " + event + ": scheduled at "
+                    + showTime(event));
+            if (++count >= 5) break;
+        }
+        if (mEventQueue.size() > count) {
+            Log.d(TAG, "     .....");
+        }
+    }
+
+    private synchronized void execute() {
+        if (stopped()) return;
+
+        MyEvent firstEvent = mEventQueue.poll();
+        if (firstEvent != null) {
+            Log.d(TAG, "execute " + firstEvent + " at "
+                    + showTime(firstEvent));
+            // run the callback in a new thread to prevent deadlock
+            new Thread(firstEvent.mCallback).start();
+
+            // TODO: fire all the events that are already late
+            scheduleNext();
+        }
+        printQueue();
+    }
+
+    private String getAction() {
+        return toString();
+    }
+
+    private static class MyEvent {
+        PendingIntent mPendingIntent;
+        long mTriggerTime;
+        Runnable mCallback;
+    }
+
+    private static class MyEventComparator implements Comparator<MyEvent> {
+        public int compare(MyEvent e1, MyEvent e2) {
+            long diff = e1.mTriggerTime - e2.mTriggerTime;
+            return (diff > 0) ? 1 : ((diff == 0) ? 0 : -1);
+        }
+
+        public boolean equals(Object that) {
+            return (this == that);
+        }
+    }
+
+    private static long showTime() {
+        return showTime(SystemClock.elapsedRealtime());
+    }
+
+    private static long showTime(MyEvent event) {
+        return showTime(event.mTriggerTime);
+    }
+
+    private static long showTime(long time) {
+        return (time / 1000 % 10000);
+    }
 }
